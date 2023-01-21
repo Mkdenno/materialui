1836d5f653c10646973cee24dd52e14f043edd49
denniskiprop25@gmail.com <dennis kiprop masiror>
2023-01-21 11:23:09 +0300

    It will add rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,10 +1,58 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from "@mui/material";
+import React from "react";
 
 const Rightbar = () => {
   return (
-    <Box bgcolor="blue" flex={2} p={2} sx={{display:{xs:"none", sm:"block" }}}>RightBAr</Box>
-  )
-}
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position={"fixed"}>
+        <Typography fontWeight={100} variant="h6">
+          Online Friends
+        </Typography>
+        <AvatarGroup max={4}>
+          <Avatar
+            alt="Repy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://mui.com/static/images/avatar/4.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://mui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://mui.com/static/images/avatar/1.jpg"
+          />
+        </AvatarGroup>
 
-export default Rightbar 
+        <Typography fontWeight={100} variant="h6">
+          Latest Photos
+        </Typography>
+
+        <ImageList  cols={3} rowHeight={164}>
+          <ImageListItem >
+            <img src="https://images.unsplash.com/photo-1597645587822-e99fa5d45d25?w=164&h=164&fit=crop&auto=format" alt="" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1597645587822-e99fa5d45d25?w=164&h=164&fit=crop&auto=format" alt="" />
+          </ImageListItem>
+
+         <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1597645587822-e99fa5d45d25?w=164&h=164&fit=crop&auto=format" alt="" />
+          </ImageListItem> 
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1597645587822-e99fa5d45d25?w=164&h=164&fit=crop&auto=format" alt="" />
+          </ImageListItem>
+        </ImageList>
+      </Box>
+    </Box>
+  );
+};
+
+export default Rightbar;
